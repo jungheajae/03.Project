a616b1476a6d2bada8c783fd1349507e26157cfc
jungheajae <facerainbow@naver.com>
2021-10-04 12:09:31 +0900

    first commit

---
+++ 04-chrome-app-momentum/app.js
@@ -0,0 +1,34 @@
+// number / string / const / let
+
+const a = 2;
+const b = 9;
+let myName = "heajae";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hi " + myName);
+
+myName = "jung hea jae";
+
+console.log("my new name is " + myName);
+
+// true&false / null / undefined
+
+const amISlim = true;
+const amIFat = false;
+const cloud = null;
+let something;
+console.log(something, cloud, amISlim, amIFat);
+
+// array
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri"];
+
+console.log(daysOfWeek);
+
+console.log(daysOfWeek[4]);
+
+daysOfWeek.push("sat", "sun");
+
+console.log(daysOfWeek);
