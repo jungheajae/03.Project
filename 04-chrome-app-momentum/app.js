8c80e6be2dbddd0f3e5bb52e0c117bb5b4739e2a
jungheajae <facerainbow@naver.com>
2021-10-05 11:08:16 +0900

    second commit

--- 04-chrome-app-momentum/app.js
+++ 04-chrome-app-momentum/app.js
@@ -32,3 +32,46 @@ console.log(daysOfWeek[4]);
 daysOfWeek.push("sat", "sun");
 
 console.log(daysOfWeek);
+
+// object
+
+const player = {
+  name: "heajae",
+  age: 21,
+  slim: true,
+};
+
+console.log(player);
+console.log(player.name);
+
+player.lastname = "jung";
+player.slim = "false";
+player.age = player.age + 10;
+console.log(player);
+
+// 적용해보기
+
+let people = "jung hea jae";
+const age = 31;
+
+console.log(people + " is " + age + " years old");
+
+people = "nam so yeon";
+
+console.log(people + " is " + age + " years old");
+
+const amIPretty = true;
+const amIUgly = false;
+const myBrain = null;
+let myBody;
+
+console.log(amIPretty, amIUgly, myBrain, myBody);
+
+const myFavoriteFood = ["sushi", "noodle", "soup"];
+
+console.log(myFavoriteFood);
+console.log(myFavoriteFood[2]);
+
+myFavoriteFood.push("chicken", "pizza");
+
+console.log(myFavoriteFood[4]);
